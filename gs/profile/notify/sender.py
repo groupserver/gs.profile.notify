355e8b12271f0c7545c6b202a8687a524307c5b7
Michael JasonSmith <mpj17@onlinegroups.net>
2011-05-17 12:18:32 +1000

    Sorting out the quoting, and bugfixing

--- gs/profile/notify/sender.py
+++ gs/profile/notify/sender.py
@@ -6,25 +6,26 @@ from email.MIMEMultipart import MIMEMultipart
 from email.utils import formataddr
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('groupserver')
-from Products.XWFCore.XWFUils import get_support_email
-from Products.CustomUserFolder.interfaces.IGSUserInfo
+from Products.XWFCore.XWFUtils import get_support_email
+from Products.CustomUserFolder.interfaces import IGSUserInfo
 from notifyuser import NotifyUser
+utf8 = 'utf-8'
 
 class MessageSender(object):
     def __init__(self, context, toUserInfo):
         self.context = context
         self.toUserInfo = toUserInfo
-        self.notifyUser = NotifyUser(toUserInfo.userObj)
+        self.notifyUser = NotifyUser(toUserInfo.user)
         
     def send_message(self, subject, txtMessage, htmlMessage='', 
                         fromAddress=None, toAddresses=None):
         msg = self.create_message(subject, txtMessage, htmlMessage, 
                                   fromAddress, toAddresses)
+        print msg
         if not toAddresses:
             toAddresses = self.notifyUser.addresses
-        t = msg.as_string()
         for addr in toAddresses:
-            self.notifyUser.send_message(t, addr, fromAddress)
+            self.notifyUser.send_message(msg, addr, fromAddress)
             
     def create_message(self, subject, txtMessage, htmlMessage, 
                         fromAddress, toAddresses):
@@ -33,28 +34,33 @@ class MessageSender(object):
         container['From'] = self.from_header_from_address(fromAddress)
         container['To'] = self.to_header_from_addresses(toAddresses)
 
-        txt = MIMEText(t.encode(utf8), 'plain', utf8)
+        txt = MIMEText(txtMessage.encode(utf8), 'plain', utf8)
         container.attach(txt)
         
         if htmlMessage:
+            # --=mpj17=-- I checked, and it is valid to have a 
+            # multipart message with only one part: "The body must then 
+            # contain one or more body parts\ldots"
+            # <http://tools.ietf.org/html/rfc2046#section-5.1>
             html = MIMEText(htmlMessage.encode(utf8), 'html', utf8)
             container.attach(html)
-
-        return container
+        retval = container.as_string()
+        assert retval
+        return retval
 
     def from_header_from_address(self, address):
         if address:
             u = self.context.acl_users.get_userByEmail(address)
             assert u, 'Could not find user for <%s>' % address
             userInfo = IGSUserInfo(u)
-            retval = formataddr((userInfo.name, address))
+            retval = formataddr((userInfo.name.encode(utf8), address))
         else:
             siteInfo = createObject('groupserver.SiteInfo', self.context)
             siteId = siteInfo.id
             assert siteId, 'Could not get the site ID'
             name = siteInfo.name +_(' Support')
             email = get_support_email(self.context, siteId)
-            retval = formatadddr((name, email))
+            retval = formatadddr((name.encode(utf8), email))
         assert retval
         return retval
 
@@ -63,8 +69,9 @@ class MessageSender(object):
             addresses = self.notifyUser.addresses
         assert addresses, 'No addresses for %s (%s)' % \
             (self.toUserInfo.name, self.toUserInfo.id)
-        fn = self.toUserInfo.name
-        retval = ', '.join([formataddr((fn, a)) for a in addresses])
+        fn = self.toUserInfo.name.encode(utf8)
+        retval = ', '.join([formataddr((fn.encode(utf8), a)) 
+                            for a in addresses])
         assert retval
         return retval
 
