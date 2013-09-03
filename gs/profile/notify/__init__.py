4642a239d95c8d3fe526c47b7a27ae15540102bf
Michael JasonSmith <mpj17@onlinegroups.net>
2013-09-03 15:10:38 -0700

    Ensuring that the gs.profile.notify.NotifyUser is exproted to the ZMI-side scripts.

--- gs/profile/notify/__init__.py
+++ gs/profile/notify/__init__.py
@@ -1,3 +1,24 @@
-# coding=utf-8
-from notifyuser import NotifyUser 
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+#lint:disable
+from notifyuser import NotifyUser
 from sender import MessageSender
+#lint:enable
+
+from AccessControl import ModuleSecurityInfo
+from AccessControl import allow_class
+m_security = ModuleSecurityInfo('gs.profile.notify.notifyuser')
+m_security.declarePublic('NotifyUser')
+allow_class(NotifyUser)
