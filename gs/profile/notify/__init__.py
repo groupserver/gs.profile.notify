1637fb37bac71947f4f37e67d673daa7756ba181
Michael JasonSmith <mpj17@onlinegroups.net>
2014-09-11 17:50:36 -0700

    Adding an abstract base class for a notifier.

--- gs/profile/notify/__init__.py
+++ gs/profile/notify/__init__.py
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-# Copyright © 2013 OnlineGroups.net and Contributors.
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE.
-#
-##############################################################################
 from __future__ import absolute_import, unicode_literals
 #lint:disable
+from .notifier import NotifierABC
 from .notifyuser import NotifyUser
 from .sender import MessageSender
 #lint:enable
