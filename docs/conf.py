4457d34990902e43a99e17a2fc359eb6f79ea492
Michael JasonSmith <mpj17@onlinegroups.net>
2015-06-24 11:35:03 -0700

    Huh, intersphinx actually works.

--- docs/conf.py
+++ docs/conf.py
@@ -362,4 +362,8 @@ epub_exclude_files = ['search.html']
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/2.7', None),
+    'gsemail': ('http://groupserver.readthedocs.org/projects/gsemail/en/latest/', None),
+    'gscontentemailbase': ('http://groupserver.readthedocs.org/projects/gscontentemailbase/en/latest/', None),
+}
